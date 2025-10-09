cd09f36ecde5609a72a523a25ed45c53e852dff3
nsfisis <nsfisis@gmail.com>
2025-10-09 23:05:06 +0900

    feat(claude-code): update

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -9,11 +9,11 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "claude-code";
-  version = "2.0.1";
+  version = "2.0.11";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-    hash = "sha256-LUbDPFa0lY74MBU4hvmYVntt6hVZy6UUZFN0iB4Eno8=";
+    hash = "sha256-h+xNtaA2X9uFlmQgaRUEXB0utIpC9FT2tn+QnnWMVSs=";
   };
 
   npmDepsHash = "sha256-DehkeMZvzn+hvcCDzJfd4p9oYc1GSZm8gu8vKS4Uncw=";
