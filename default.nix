7e3b2bfea88e28e4abba02f697689f6f4a98e573
nsfisis <nsfisis@gmail.com>
2026-02-04 21:50:16 +0900

    feat(nix): nix flake update

--- default.nix
+++ default.nix
@@ -6,7 +6,9 @@
   hgrep = pkgs.callPackage ./pkgs/hgrep { };
 
   git-helpers = pkgs.callPackage ./pkgs/git-helpers { };
-  nvim-setcellwidths-table-for-udev-gothic = pkgs.callPackage ./pkgs/nvim-setcellwidths-table-for-udev-gothic { };
+  nvim-setcellwidths-table-for-udev-gothic =
+    pkgs.callPackage ./pkgs/nvim-setcellwidths-table-for-udev-gothic
+      { };
   reparojson = pkgs.callPackage ./pkgs/reparojson { };
   term-banner = pkgs.callPackage ./pkgs/term-banner { };
   term-clock = pkgs.callPackage ./pkgs/term-clock { };
