ab3929245d3a96f71be5ef1b197efd8ebe659908
nsfisis <nsfisis@gmail.com>
2026-02-20 21:22:30 +0900

    feat(reparojson): upgrade to 0.2.0

--- pkgs/reparojson/default.nix
+++ pkgs/reparojson/default.nix
@@ -6,15 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "reparojson";
-  version = "0.1.3";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "nsfisis";
     repo = "reparojson";
     rev = "v${version}";
-    hash = "sha256-HJfzxNDFzutYluc+VKcsxxzStEaizSfynyvq0U4tEs0=";
+    hash = "sha256-dJzOR0J63sDb/gfm+H5KwdcAdqcFCHXuCtj6C6sMQK4=";
+    fetchSubmodules = true;
   };
-  cargoHash = "sha256-sSLjobm/hWpkEaRIvPshjCXNKmLlIHOsSRyBqGTshlQ=";
+  cargoHash = "sha256-Coj64lPl7o8RjKI/6CqmwiIfNnegGeG9D6/yP/zuyh0=";
 
   meta = {
     description = "A simple command-line tool to repair JSON. It only fixes the syntactic errors and never formats the given input.";
